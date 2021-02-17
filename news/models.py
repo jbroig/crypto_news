3f0f64a7a73beee795ae6e2670e79d3f83834324
Juan <juan.bravo.roig@devoteam.com>
2021-02-17 12:28:05 +0100

    Added height and width size of imgs

--- news/models.py
+++ news/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Headline(models.Model):
     title = models.CharField(max_length=200)
     image = models.URLField(null=True, blank=True)
+    image_width = models.IntegerField(null=True, blank=True)
+    image_height = models.IntegerField(null=True, blank=True)
     url = models.TextField()
 
     def __str__(self):
